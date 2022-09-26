b8de1a60a0be5767f13c883223c19dd963f0ced4
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-26 16:30:45 -0700

    added fields to category form

--- pages/categories/[id].tsx
+++ pages/categories/[id].tsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
+
 const Category = (props) => {
   const category = JSON.parse(props.category);
-  return <h1>Category {category._id}</h1>;
+  const [name, setName] = useState(category.name);
+  const [description, setDescription] = useState(category.description);
+  return (
+    <>
+      <h1>Category {category._id}</h1>
+      <form>
+        <label>Name</label>
+        <input
+          value={name}
+          onChange={(e) => {
+            return setName(e.target.value);
+          }}
+        />
+        <label>Description</label>
+        <input
+          value={description}
+          onChange={(e) => {
+            return setDescription(e.target.value);
+          }}
+        />
+      </form>
+    </>
+  );
 };
 
 export const getServerSideProps = async (context) => {
