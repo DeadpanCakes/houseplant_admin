905f9f6cd049d843995b56f6b53807062d28260c
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-20 15:07:47 -0700

    added put method

--- pages/api/products/[id].tsx
+++ pages/api/products/[id].tsx
@@ -1,17 +1,33 @@
+const DEV_API = process.env.DEV_API;
+const API = DEV_API || process.env.DIST_API;
+
 const handler = (req, res) => {
   switch (req.method) {
     case "GET":
       return get(req, res);
+    case "PUT":
+      return put(req, res);
     default:
       return res.status(400).json({ message: "Unsupported HTTP Method" });
   }
 };
 
 const get = async (req, res) => {
-  const DEV_API = process.env.DEV_API;
-  const product = await fetch(
-    (DEV_API || process.env.DIST_API) + "/products/" + req.query.id
-  ).then((res) => res.json());
+  const product = await fetch(API + "/products/" + req.query.id).then((res) =>
+    res.json()
+  );
+  res.json(product);
+};
+
+const put = async (req, res) => {
+  const options = {
+    method: "PUT",
+    body: JSON.stringify({ id: req.body._id, update: req.body }),
+    headers: { "Content-Type": "application/json" },
+  };
+  const product = await fetch(API + "/products/" + req.query.id, options).then(
+    (res) => res.json()
+  );
   res.json(product);
 };
 
